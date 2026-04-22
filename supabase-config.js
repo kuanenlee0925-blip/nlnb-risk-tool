50326a07e87807715db4464ce2c7004d4292c45f
kuanenlee0925-blip <kuanenlee0925@gmail.com>
2026-04-22 16:06:16 -0400

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 export const SUPABASE_URL = "https://fgwwxzrapymnwvypbkrfm.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_FuysHSvjuzM-4OUz9G0Bow_jkewtJ6G";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZnd3d4enJhcHltbnd5cGJrcmZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4NzUwODMsImV4cCI6MjA5MjQ1MTA4M30.cFTVzLLbLc4Y6qU-wvY67CO7HEEZXxMRjgGlP9YAghY";
 export const SUPABASE_ADMIN_EMAIL = "kxl5821@psu.edu";
 export const SUPABASE_TABLE = "question_requests";
 
