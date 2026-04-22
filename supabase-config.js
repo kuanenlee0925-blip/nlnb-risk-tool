03d52f6622d809585800bc47349493c292d023c4
kuanenlee0925-blip <kuanenlee0925@gmail.com>
2026-04-22 15:14:17 -0400

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,5 @@
-export const SUPABASE_URL = "";
-export const SUPABASE_ANON_KEY = "";
+export const SUPABASE_URL = "https://fgwwxzrapymnwvypbkrfm.supabase.co";
+export const SUPABASE_ANON_KEY = "sb_publishable_FuysHSvjuzM-4OUz9G0Bow_jkewtJ6G";
 export const SUPABASE_ADMIN_EMAIL = "kxl5821@psu.edu";
 export const SUPABASE_TABLE = "question_requests";
+
